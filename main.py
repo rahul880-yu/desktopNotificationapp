1b735b4a6983b76e70a10009e0d64886ce11597b
rahul880-yu <misterrahulchauhan@gmail.com>
2023-11-03 10:17:19 +0530

    added desktopNotfication

---
+++ main.py
@@ -0,0 +1,7 @@
+from plyer import notification
+notification.notify(
+    title="Hii, I'm here to alert you",
+    message="Don't forget to take help from me :)",
+    app_icon=None,
+    timeout=10
+)
